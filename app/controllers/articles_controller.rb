be98690a4295b9560b0636880f6f26be922d9b7f
Alex Kelley <adkelley@mac.com>
2015-06-14 15:03:19 -0700

    articles wip

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,20 @@
 class ArticlesController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    redirect :show
+  end
+
+  def edit
+    render :edit
+  end
+
+  def show
+    render :show
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,15 +13,28 @@ Rails.application.routes.draw do
 
   get '/sign_in', to: "sessions#new"
   post 'sessions', to: "sessions#create"
+  delete '/users/:id', to: "sessions#destroy", as: "session_destroy"
+
+  get 'articles/new', to: "articles#new", as: "new_article"
+  post 'articles', to: "articles#create"
+#  post 'articles', to: "articles#edit"
+#  get 'articles', to: "articles#show"
+#  delete '/users/:id', to: "articles#destroy", as: "article_destroy"
+  
 
 end
-#          Prefix Verb URI Pattern                Controller#Action
-#            root GET  /                          sites#index
-#           sites GET  /sites(.:format)           sites#index
-#     sites_about GET  /sites/about(.:format)     sites#about
-#   sites_contact GET  /sites/contact(.:format)   sites#contact
-#        new_user GET  /users/new(.:format)       users#new
-#           users POST /users(.:format)           users#create
-#                 GET  /users/:id(.:format)       users#show
-#         sign_in GET  /sign_in(.:format)         sessions#new
-# sessions_create POST /sessions/create(.:format) sessions#create
+#          Prefix Verb   URI Pattern              Controller#Action
+#            root GET    /                        sites#index
+#           sites GET    /sites(.:format)         sites#index
+#     sites_about GET    /sites/about(.:format)   sites#about
+#   sites_contact GET    /sites/contact(.:format) sites#contact
+#        new_user GET    /users/new(.:format)     users#new
+#           users POST   /users(.:format)         users#create
+#                 GET    /users/:id(.:format)     users#show
+#         sign_in GET    /sign_in(.:format)       sessions#new
+#        sessions POST   /sessions(.:format)      sessions#create
+# session_destroy DELETE /users/:id(.:format)     sessions#destroy
+#     new_article GET    /articles/new(.:format)  articles#new
+#        articles POST   /articles(.:format)      articles#create
+#                 GET    /articles(.:format)      articles#show
+# article_destroy DELETE /users/:id(.:format)     articles#destroy
